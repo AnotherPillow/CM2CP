febf5c45f9bafb8fc9f0f098c818fe0863a227fb
AnotherPillow <Thepilous_@outlook.com>
2024-02-25 13:39:19 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# CM2CP
+
+## Usage
+
+1. Download the latest release, if not present or not up to date, clone this repository with `git clone https://github.com/anotherpillow/CM2CP.git --recurse-submodules`.
+2. Install Python, 3.11 was used for the development of this.
+3. Depending on your platform and your Python install, install requirements using:
+    - `pip install -r requirements.txt`
+    - `py -m pip install -r requirements.txt`
+    - `python3 -m pip install -r requirements.txt`
+    - `python -m pip install -r requirements.txt`
+4. Place your STF mod in CM2CP's folder so that its manifest is `<CM2CP Folder>/[CM] Cool Mod/manifest.json`
+5. Rename the mod to `input`, so that the manifest is `<CM2CP Folder>/input/manifest.json`
+6. Run `main.py`, preferably through a terminal using one of the following commands:
+   - `py main.py`
+   - `python3 main.py`
+   - `python main.py`
+7. Find the converted mod in `output/`
---
+++ src/Converter.py
@@ -0,0 +1,75 @@
+import json5, shutil, os, json
+from preconditiontogsq import convert as preconditiontogsq
+
+from .Logger import logger
+
+curly_st = '{{'
+curly_end = '}}'
+
+class Converter:
+    manifest: dict = {}
+    incontent: dict = {}
+    outcontent: dict = {
+        "Format": "2.0",
+        "Changes": []
+    }
+    
+    def __init__(self):
+        self.manifest = json5.load(open('input/manifest.json', encoding='utf8'))
+        self.incontent = json5.load(open('input/content.json', encoding='utf8'))
+
+        if os.path.exists('output'):
+            shutil.rmtree('output')
+        shutil.copytree('input', 'output')
+
+    def convert(self):
+        
+        for track in self.incontent['Music']:
+            # {
+            # "Id": "title_night",
+            # "File": "select.ogg",
+            # "Loop": true,
+            # "Ambient": false
+            # }
+            entry = {
+                track['Id']: { # Ideally ID would be actually unique, but would require modifying of other parts.
+                    'ID': track['Id'], 
+                    'FilePaths': [
+                        f'{curly_st}AbsoluteFilePath: {track["File"]}{curly_end}'
+                    ],
+                    'Category': 'Ambient' if track['Ambient'] == True else 'Default',
+                    'Looped': track['Loop']
+                }
+            }
+        
+
+            change = {
+                "Action": "EditData",
+                "Target": "Data/Shops",
+                "Entries": entry
+            }
+
+            self.outcontent['Changes'].append(change)
+
+
+
+        self.translateManifest()
+        self.save()
+
+
+    def translateManifest(self):
+        self.manifest['UniqueID'] += '.CP'
+        self.manifest['Author'] += ' ~ CM2CP'
+
+        self.manifest['ContentPackFor']['UniqueID'] = 'Pathoschild.ContentPatcher'
+        if 'Dependencies' in self.manifest:
+            self.manifest['Dependencies'] = \
+                [mod for mod in self.manifest['Dependencies'] if mod['UniqueID'] not in ['Cherry.ShopTileFramework']]
+        
+    def save(self):
+        
+        with open('output/manifest.json', 'w') as f:
+            json.dump(self.manifest, f, indent=4)
+        
+        with open('output/content.json', 'w') as f:
+            json.dump(self.outcontent, f, indent=4)
